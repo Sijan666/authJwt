771bb9e2b39b00718b952c2acb14deddabd77745
Sijan666 <666majharulislam@gmail.com>
2026-08-15 13:35:04 +0600

    refactor: clean up auth routes using new controller and middleware

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,81 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require("nodemailer");
-const otpGenerator = require('otp-generator')
-const User = require('../models/userSchema')
-const permission = require('../permission')
-var jwt = require('jsonwebtoken');
+const checkAccess = require('../middleware/authMiddleware');
+const { registerUser, loginUser, getPrivateData } = require('../controllers/authController'); 
 
-// registration
-router.post('/registration' , async (req,res) => {
+router.post('/registration', registerUser);
+router.post('/login', loginUser);
+router.get("/privatedata", checkAccess, getPrivateData);
 
-    let {role='student',email} = req.body
-
-    let per;
-
-    permission.map(item=>{
-        if(item.role == role){
-            per = item.permission
-        }
-    })
-
-    console.log(per);
-    
-    const user = await new User({
-        email : email,
-        role : role,
-        permission : per
-    }).save()
-
-    res.json({
-        data : user
-    })
-    
-})
-
-// login
-router.post('/login' , async (req,res) => {
-    let {email} = req.body
-
-    const existingUser = await User.findOne({email:email})
-
-    console.log(existingUser);
-    
-    // jwt.sign({data,secret,expire})
-
-    let token = jwt.sign({
-        _id : existingUser.id,
-        email : existingUser.email,
-        role : existingUser.role,
-        permission : existingUser.permission
-    },process.env.JWT_SECRET_ACCESS,{
-        expiresIn : '1h'
-    })
-
-    res.json({
-        accessToken : token
-    })
-})
-
-function abc(req,res,next) {
-    let token = req.headers.authorization;
-    
-    console.log(token.split(' ')[1]);
-
-    jwt.verify(token.split(' ')[1],process.env.JWT_SECRET_ACCESS, function(err, decoded) {
-        if(decoded.role == "student"){
-            res.send('you do not have access')
-        }
-        else{
-            next()
-        }
-    });
-    
-}
-
-// privateData
-router.get("/privatedata" , abc, async (req,res) => {
-    await res.send('data')
-})
+module.exports = router;
 
 module.exports = router
