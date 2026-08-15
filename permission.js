397d112ba4fd80a616d1aed4f241ab8c53a4af46
Sijan666 <666majharulislam@gmail.com>
2026-08-15 13:45:24 +0600

    refactor : update permissions list

--- permission.js
+++ permission.js
@@ -1,15 +1,23 @@
 let permission  = [
     {
         role : 'student',
-        permission : ['view']
+        permission : [
+            'view_profile', 
+            'view_materials', 
+            'submit_assignment', 
+        ]
     },
     {
         role : 'teacher',
-        permission : ['edit']
+        permission : [
+            'create_materials', 
+            'update_materials', 
+            'delete_own_materials',
+        ]
     },
     {
         role : 'admin',
-        permission : ['all']
+        permission : ['all'] 
     }
 ]
 
